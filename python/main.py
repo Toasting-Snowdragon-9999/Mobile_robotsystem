1cf2efb2e3ee6fb714ab01828f72250c9a19fb35
Christoper <phfae23@student.sdu.dk>
2024-10-06 13:08:41 +0200

    Python is now being run from the correct dir

--- python/main.py
+++ python/main.py
@@ -1,6 +1,35 @@
+import os
 from packages.share_data import SharedData
 
+def path():
+    try:
+        # Get the current user's home directory
+        home_dir = os.path.expanduser('~')
+
+        # Initialize target_dir as None
+        target_dir = None
+
+        # Walk through the home directory and subdirectories to find 'Mobile_robotsystem'
+        for root, dirs, files in os.walk(home_dir):
+            if 'Mobile_robotsystem' in dirs:
+                target_dir = os.path.join(root, 'Mobile_robotsystem')
+                break
+
+        # Check if the target directory was found
+        if target_dir:
+            os.chdir(target_dir)
+            print(f"Changed working directory to: {os.getcwd()}")
+        else:
+            print("Mobile_robotsystem directory not found.")
+
+    except Exception as e:
+        print(f"Failed to change directory: {e}")
+        return
+
 def main(): 
+    
+    path()
+    print(os.getcwd())
     data = [
         [11, 8, 5],
         [14, 7, 7],
