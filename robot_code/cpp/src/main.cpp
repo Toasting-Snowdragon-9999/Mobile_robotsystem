e34aa35f900a9a81569ab8aa3ea9d08f7d3d99b6
Sarah <sarah-rousing@live.dk>
2024-10-30 10:13:37 +0100

    idk

--- robot_code/cpp/src/main.cpp
+++ robot_code/cpp/src/main.cpp
@@ -3,35 +3,18 @@
 #include "algorithms.h"
 
 int main(){
-	// SharedData sd;
 
-	// //lyde
-	// std::vector<std::vector<int>> sequence1 = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15},{16,0,0}}; //missing 16
-    // std::vector<std::vector<int>> sequence2 = {{2,7,4},{16,1,2},{14,8,6},{1,3,9},{15,3,4},{9,16,1}};
-    // std::vector<std::vector<int>> sequence3 = {{16,16,16},{1,1,1},{16,16,16},{1,1,1},{16,16,16}};
-    // std::vector<std::vector<int>> sequence4 = {{1,2,3}};
+	std::vector<std::complex<double>> a = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+	SP::Algorithms<double> A(a);
+	SP::Algorithms<double> B;
+	A.print_data();
 
-	// WaveGenerator sounds(sequence1);
-	// sounds.play_sounds();
-	// //lyde ended
+	std::cout << "\nFFT: " << std::endl;
+	B.FFT(a);
+	B.print_data();
 
-	// //py to cpp
-	// while (1){
-	// 	try{
-	// 		sd.read_shared_data();
-	// 		sd.print();
-	// 	}
-	// 	catch(SharedDataException &e){
-	// 		if (e.error_code() == 21){
-
-	// 		}
-	// 		else{std::cout << "[Error] " << e.what() << std::endl;}
-	// 	}
-	// }
-	// //py to cpp ended
-
-	SP::Algorithms<int> A(10);
-	A.print();
+	std::cout << "\nGoertzel: " << std::endl;
+	A.goertzel();
 
 	return 0;
 }
