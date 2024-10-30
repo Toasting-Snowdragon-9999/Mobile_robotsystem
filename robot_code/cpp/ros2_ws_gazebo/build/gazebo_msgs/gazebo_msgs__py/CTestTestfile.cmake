de00e3467f290d5e722ee4327938d03aa704b3fb
christopher <phfae23@student.sdu.dk>
2024-10-30 09:03:45 +0100

    Ros code start

---
+++ robot_code/cpp/ros2_ws_gazebo/build/gazebo_msgs/gazebo_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chris/git/Mobile_robotsystem/robot_code/cpp/ros2_ws_gazebo/build/gazebo_msgs/gazebo_msgs__py
+# Build directory: /home/chris/git/Mobile_robotsystem/robot_code/cpp/ros2_ws_gazebo/build/gazebo_msgs/gazebo_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ robot_code/cpp/ros2_ws_gazebo/build/turtlebot3_fake_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chris/git/Mobile_robotsystem/robot_code/cpp/ros2_ws_gazebo/src/turtlebot3_simulations/turtlebot3_fake_node
+# Build directory: /home/chris/git/Mobile_robotsystem/robot_code/cpp/ros2_ws_gazebo/build/turtlebot3_fake_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
