9d76702337a52eaef52efb2d9ac9539d07aa08c9
jarmari <jhmar23@student.sdu.dk>
2024-11-03 22:55:16 +0100

    Modified conversion to handle more than 32 bits

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -6,7 +6,9 @@
 #include <algorithm>
 #include <string>
 
-using std::string, std::cout, std::endl;
+using std::cout;
+using std::endl;
+using std::string;
 
 /**
  * @brief Binary conversion for CRC-check
@@ -14,11 +16,10 @@ using std::string, std::cout, std::endl;
  * @param decimalSequence The original tone sequence in decimal, divided in vectors of vectors
  * @return The sequence in a long binary string
  */
-string decimalSeqToBinaryMsg(std::vector<std::vector<int>> decimalSequence)
+string decimalSeqToBinaryMsg(const std::vector<std::vector<int>>& decimalSequence)
 {
-	int binaryConvertedTone = 0;
-	std::string binaryConvertedToneStr;
-	std::vector<int> buffer;
+	string binaryConvertedTone = "";
+	std::string tmpTone;
 
 	for (auto pair : decimalSequence)
 	{
@@ -26,47 +27,33 @@ string decimalSeqToBinaryMsg(std::vector<std::vector<int>> decimalSequence)
 		{
 			int toneCopy = tone;
 			if (tone == 0)
-				binaryConvertedTone = binaryConvertedTone << 1 | 0; // If a tone is zero, no conversion is needed
-
-			while (tone != 0)
 			{
-				buffer.push_back(tone % 2);
-				tone /= 2;
+				tmpTone += '0'; // If a tone is zero, no conversion is needed
+
+				continue;
 			}
 
-			if (toneCopy > 0) // Reverse buffer to get MSB to LSB
+			while (toneCopy > 0)
 			{
-				std::reverse(buffer.begin(), buffer.end());
-				binaryConvertedTone = binaryConvertedTone << 1 | buffer[0];
+				tmpTone += (toneCopy % 2) ? '1' : '0';
 
-				for (int i = 0; i < buffer.size() - 1; i++)
-				{
-					binaryConvertedTone = binaryConvertedTone << 1 | buffer[i + 1];
-				}
+				toneCopy /= 2;
 			}
-			buffer.clear();
+
+			std::reverse(tmpTone.begin(), tmpTone.end());
+			binaryConvertedTone += tmpTone;
+			tmpTone.clear();
 		}
 	}
 
-	int actualBitLength = 0;
-	int temp = binaryConvertedTone;
-	while (temp > 0)
-	{ // Find extra zeroes in binary conversion
-		temp >>= 1;
-		actualBitLength++;
-	}
-
-	std::bitset<32> redundantConvertedTone(binaryConvertedTone);
-	binaryConvertedToneStr = redundantConvertedTone.to_string().substr(32 - actualBitLength);
-
-	return binaryConvertedToneStr;
+	return binaryConvertedTone;
 }
 
 /// @brief Performs XOR-operation on 2 strings characterwise
 /// @param a
 /// @param b
 /// @return Result of XOR-operation
-string ExclusiveOR(string a, string b)
+string ExclusiveORStrings(string a, string b)
 {
 	string xorresult = "";
 	if (a.size() != b.size())
@@ -75,54 +62,65 @@ string ExclusiveOR(string a, string b)
 	}
 	for (int i = 0; i < a.size(); i++)
 	{
-		(a[i] == b[i]) ? '0' : '1';
+		xorresult += (a[i] == b[i]) ? '0' : '1';
 	}
 	return xorresult;
 }
 
-/// @brief Encode binary data sequence with CRC4-Codeword
-/// @param uncodedBinarySequence
-/// @return Binary sequence with CRC4-Codeword appended
-string CRCencode(string uncodedBinarySequence)
+/// @brief Encode binary dataword with CRC4-Codeword
+/// @param binaryDataword
+/// @return Binary dataword with CRC4-Codeword appended
+string CRC4Encode(string binaryDataword)
 {
-	string CRCFourCodeword = "10011";
-	string remainder = "";
-	string &remainderRef = remainder;
-	int codewordLength = CRCFourCodeword.length();
-	string zeroes = (codewordLength, "0");
-	;
-	int datawordLength = uncodedBinarySequence.length();
-	std::string::iterator subsIndexIt = uncodedBinarySequence.begin();
-	int subsIndexInt = std::distance(uncodedBinarySequence.begin(), subsIndexIt);
-	int subsIndexInt2 = codewordLength;
-	int lengthWithoutCodeword = std::distance(uncodedBinarySequence.begin(), (uncodedBinarySequence.end() - codewordLength));
-	int bitshiftCounter = 0;
-	int firstCounter = 0;
+	string codeword = "10011";
+	string encodedBinaryData = binaryDataword;
 
-	uncodedBinarySequence.append(CRCFourZeroes); // Append zeroes to data
+	int crcDegree = codeword.length() - 1;
 
-	while (subsIndexInt <= lengthWithoutCodeword) // Binary-division
+	int selectionPlusOneIdx = codeword.length();
+	int codewordLength = codeword.length();
+
+	string binaryDatawordWithZeroes = binaryDataword + string(crcDegree, '0'); // Append CRC-Degree zeroes to data
+
+	string selection = binaryDatawordWithZeroes.substr(0, codeword.length());
+	int datawordLength = binaryDatawordWithZeroes.length();
+
+	while (selectionPlusOneIdx < datawordLength) // Binary-division
 	{
-
-		for (int i = 0; i < CRCFourCodeword.length(); i++) // XOR on Substring
+		if (selection[0] == '1')
 		{
-			if (firstCounter <= 0) // XOR on the first string
-			{
-				remainderRef.append(uncodedBinarySequence.substr(subsIndexInt2, bitshiftCounter));
-				remainderRef.append(ExclusiveOR(uncodedBinarySequence.substr(subsIndexInt, subsIndexInt2)[], CRCFourCodeword[j]));
-			}
-			else
-			{
-			}
+			selection = ExclusiveORStrings(selection, codeword);
 		}
 
-		if (remainder[0] == 0)
-		{
-			for (int i = 0)
-		}
+		selection = selection.substr(1) + binaryDataword[selectionPlusOneIdx];
+		selectionPlusOneIdx++;
 	}
 
-	return remainder;
+	if ((selection[0] == '1')) // XOR the last selection with the codeword if needed
+	{
+		selection = ExclusiveORStrings(selection, codeword);
+	}
+
+	string remainder = selection.substr(1); // Return substring since codeword is CRC-Degree+1 in size
+
+	return encodedBinaryData = binaryDataword + remainder;
+}
+
+// Function to print a vector of chars
+void printNestedVector(const std::vector<std::vector<int>> &preambleSeq, const std::string &name = "Preamble Sequence")
+{
+	std::cout << name << ":" << std::endl;
+	std::cout << "[" << std::endl;
+	for (const std::vector<int> &innerVec : preambleSeq)
+	{
+		std::cout << "  [ ";
+		for (int num : innerVec)
+		{
+			std::cout << num << " ";
+		}
+		std::cout << "]" << std::endl;
+	}
+	std::cout << "]" << std::endl;
 }
 
 int main()
@@ -130,12 +128,12 @@ int main()
 	SharedData sd;
 
 	// lyde
-	std::vector<std::vector<int>> sequence1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}, {13, 14, 15}, {16, 0, 0}}; // missing 16
-	std::vector<std::vector<int>> sequence2 = {{2, 7, 4}, {16, 1, 2}, {14, 8, 6}, {1, 3, 9}, {15, 3, 4}, {9, 16, 1}};
-	std::vector<std::vector<int>> sequence3 = {{16, 16, 16}, {1, 1, 1}, {16, 16, 16}, {1, 1, 1}, {16, 16, 16}};
-	std::vector<std::vector<int>> sequence4 = {{11, 8, 10}};
-	std::vector<std::vector<int>> testSequence1 = {{1}};
-	std::vector<std::vector<int>> preambleSeq = {{10, 10, 15}};
+	// std::vector<std::vector<int>> sequence1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}, {13, 14, 15}, {16, 0, 0}}; // missing 16
+	// std::vector<std::vector<int>> sequence2 = {{2, 7, 4}, {16, 1, 2}, {14, 8, 6}, {1, 3, 9}, {15, 3, 4}, {9, 16, 1}};
+	// std::vector<std::vector<int>> sequence3 = {{16, 16, 16}, {1, 1, 1}, {16, 16, 16}, {1, 1, 1}, {16, 16, 16}};
+	// std::vector<std::vector<int>> sequence4 = {{11, 8, 10}};
+	std::vector<std::vector<int>> testSequence1 = {{10, 10, 10}, {12, 9, 9, 9}, {14, 4, 5}, {13, 2, 0, 0}, {10, 10, 10}};
+	std::vector<std::vector<int>> preambleSeq = {{10}};
 
 	// WaveGenerator sounds(testSequence1);
 	// sounds.play_sounds();
@@ -143,21 +141,9 @@ int main()
 	// WaveGenerator preamble(preambleSeq);
 	// preamble.play_sounds();
 
-	// int crcDivisor = 0b10011;
-
-	// Test af binary conversion
-	std::cout << decimalSeqToBinaryMsg(preambleSeq) << std::endl;
-
-	string teststring1 = "101010";
-	string teststring2 = "010101";
-	string xorresult;
-
-	for (int i = 0; i < teststring1.length(); i++)
-	{
-		xorresult.append(ExclusiveOR(teststring1[i], teststring2[i]));
-	}
-
-	CRCencode(decimalSeqToBinaryMsg(testSequence1));
+	printNestedVector(preambleSeq, "Preamble Sequence");
+	std::cout << "Converted binary msg: " << decimalSeqToBinaryMsg(preambleSeq) << endl;
+	std::cout << "CRC4-Encoded msg: " << CRC4Encode(decimalSeqToBinaryMsg(preambleSeq)) << std::endl;
 
 	// lyde ended
 
