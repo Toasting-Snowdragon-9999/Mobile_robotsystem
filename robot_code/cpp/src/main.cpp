04f751b8403d580c5bc8e86d8208eba92945ef6f
Ayman <aymante@gmail.com>
2024-11-11 22:36:44 +0100

    Updated_size_test for FFT

--- robot_code/cpp/src/main.cpp
+++ robot_code/cpp/src/main.cpp
@@ -28,10 +28,11 @@ int main() {
 
             std::string sti = "../dtmf_sounds/Signals_for_Size_test/";
 
-            std::vector<std::string> file = {   //"output_400.txt",
-                                            "output_500.txt", "output_600.txt", "output_700.txt", "output_800.txt", "output_900.txt", 
-                                            "output_1000.txt", "output_2000.txt", "output_3000.txt", "output_4000.txt", "output_5000.txt", 
-                                            "output_6000.txt", "output_7000.txt", "output_8000.txt", "output_9000.txt", "output_10000.txt"
+            std::vector<std::string> file = {"output_400 (copy).txt"
+       /*Goertzel er ustabil ved 400 Dog er DFT*/                                     //"output_400.txt", 
+                                            //"output_500.txt", "output_600.txt", "output_700.txt", "output_800.txt", "output_900.txt", 
+                                            //"output_1000.txt", "output_2000.txt", "output_3000.txt", "output_4000.txt", "output_5000.txt", 
+                                            //"output_6000.txt", "output_7000.txt", "output_8000.txt", "output_9000.txt", "output_10000.txt"
     /*For FFT Grænsen 16394*/             //"output_16000.txt", "output_16200.txt", "output_16300.txt", "output_16384.txt", "output_17000.txt", 
                                             //"output_18000.txt", "output_19000.txt", "output_20000.txt, "output_21000.txt, "output_22000.txt"
 
@@ -42,8 +43,8 @@ int main() {
             for (int j = 0 ; j < file.size(); j++){
                 std::string full_path_file = sti + file[j];
 
-                DFT dft;
-                //Goertzel goertzel;
+                //DFT dft;
+                Goertzel goertzel;
 
                 //FFT fftProcessor;
 
@@ -51,11 +52,11 @@ int main() {
                 for(int i = 0 ; i < 30 ; ++i){
 
                     auto start_goertzel = std::chrono::high_resolution_clock::now();
-                    //goertzel.read_from_file(full_path_file);
-                    //goertzel.translate_signal_goertzel();
+                    goertzel.read_from_file(full_path_file);
+                    goertzel.translate_signal_goertzel();
 
-                    dft.read_from_file(full_path_file);
-                    dft.frequencies_of_signal();
+                    //dft.read_from_file(full_path_file);
+                    //dft.frequencies_of_signal();
 
 
                     //fftProcessor.read_from_file(full_path_file);
@@ -68,7 +69,7 @@ int main() {
                     //sleep(1);
                 }
                 //std::cout << "Time for Goertzel: " << sum/30 << " microseconds " << std::endl;
-            std::cout << sum << std::endl;
+            std::cout << sum/30 << std::endl;
             }
  
     /*-------------------------------For-loop for time_testing----------------------------------*/
