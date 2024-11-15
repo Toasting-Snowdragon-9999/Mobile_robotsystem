ac4ce6aee9aafa9b7654302fa66d47ae2cdad53f
jarmari <jhmar23@student.sdu.dk>
2024-11-15 20:02:02 +0100

    Modified zero_pad function

--- cpp/src/al_to_dll.cpp
+++ cpp/src/al_to_dll.cpp
@@ -4,15 +4,28 @@ string AlToDll::zero_pad(string &non_zero_padded_commands)
 {
     int string_length = non_zero_padded_commands.length();
 
-    int zeros_int;
+    int zeros_int = 0;
 
-    if(!(string_length % nibble_size)){
+    if (!(string_length % nibble_size))
+    {
 
-        zeros_int = nibble_size*(1+(string_length/nibble_size))-string_length;
+        zeros_int = nibble_size * (1 + (string_length / nibble_size)) - string_length;
     }
 
-    string zeros_str(zeros_int,'0');
+    std::bitset <nibble_size> how_many_zeros(zeros_int);
+
+    string how_many_zeros_str = how_many_zeros.to_string();
+
+    string zeros_str(zeros_int, '0');
 
     non_zero_padded_commands += zeros_str;
 
-};
+    how_many_zeros_str += non_zero_padded_commands;
+
+    return how_many_zeros_str;
+}
+
+    string AlToDll::remove_zero_pad(string &possibly_zero_padded_commands)
+{
+
+}
