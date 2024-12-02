21c2ff68b4eb647bd17c01d43f9274ca38937b32
christopher <phfae23@student.sdu.dk>
2024-12-02 17:40:58 +0100

    Removed ros2_ws

--- robot_code/cpp/ros2_ws/build/turtlebot3_control/CMakeFiles/move_turtlebot.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for move_turtlebot.
-# This may be replaced when dependencies are built.
--- robot_code/cpp/ros2_ws/build/turtlebot3_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/git/Mobile_robotsystem/robot_code/cpp/ros2_ws/src/turtlebot3_control
-# Build directory: /home/chris/git/Mobile_robotsystem/robot_code/cpp/ros2_ws/build/turtlebot3_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
