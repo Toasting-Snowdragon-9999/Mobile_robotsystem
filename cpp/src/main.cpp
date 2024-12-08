c31407da3a8a1232eca0828ccaaa235670a5b7e4
christopher <phfae23@student.sdu.dk>
2024-12-08 14:53:07 +0100

    hacky fix

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -27,6 +27,7 @@
 
 int main()
 {
+	TlToDll i_tl;
 	std::string ack_commands = "1";
 	for(int i = 0; i < 2; i++){
 		PhysicalLayer pl(16000, 2);
@@ -39,34 +40,37 @@ int main()
 		std::string package = dl_layer.get_data_from_package(binary_msg);
 
 		if(!package.empty()){
-			TlToDll i_tl;
 			i_tl.add_segment_to_buffer(package);
-			std::string segment = i_tl.take_segment_from_buffer();
-
-			AlToTl i_al;
-			i_al.add_string_to_buffer(segment);
-			std::string buffer = i_al.get_buffer();	
-
-			Transport_Layer tp_layer;
-			std::string package_no_header = tp_layer.remove_header_and_unstuff(buffer);
+		
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			
-			ApplicationLayer app_layer;
-			std::string final_package = app_layer.check_crc(package_no_header);
-			if(final_package.empty()){
-				std::cerr << "CRC check failed" << std::endl;
-			}
-			else{
-				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-				std::vector<robot_command> comd2 = app_layer.bits_to_commands(final_package);
-				
-				DataLinkLayer dll_ack(ack_commands);
-				dll_ack.protocol_structure();
-				std::string message_ready = dll_ack.get_ready_for_pl_path();
-				SignalProcessing sp;
-				std::vector<int> dtmf_ack = sp.convert_to_dtmf(message_ready);
-				pl.yell(dtmf_ack);
-			}
+			DataLinkLayer dll_ack(ack_commands);
+			dll_ack.protocol_structure();
+			std::string message_ready = dll_ack.get_ready_for_pl_path();
+			SignalProcessing sp;
+			std::vector<int> dtmf_ack = sp.convert_to_dtmf(message_ready);
+			pl.yell(dtmf_ack);
 		}
 	}
+	Transport_Layer tp_layer;
+
+	while(!i_tl.is_buffer_empty()){
+		std::string segment = i_tl.take_segment_from_buffer();
+		i_tl.remove_segment_from_buffer();
+		tp_layer.add_segment(segment);
+	}
+	std::string recieved = tp_layer.combine_segments_to_string();
+
+	std::string package_no_header = tp_layer.remove_header_and_unstuff(recieved);
+	
+	ApplicationLayer app_layer;
+	std::string final_package = app_layer.check_crc(package_no_header);
+	
+	std::vector<robot_command> comd2 = app_layer.bits_to_commands(final_package);
+	std::vector<std::vector<std::string>> commands = app_layer.cpp_to_robot(comd2);
+	for(auto command : commands){
+		std::cout << "Command: " << command[0] << "   Value: " << command[1] << std::endl;
+	}
+
 	return 0;
 }
